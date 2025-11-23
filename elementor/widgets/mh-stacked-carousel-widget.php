20e2b1617bd17736ee4fa3016a39e26c7f1fb87a
md-jihan <monabborhossen@gmail.com>
2025-11-24 02:42:40 +0600

    update stacked slider widget

--- elementor/widgets/mh-stacked-carousel-widget.php
+++ elementor/widgets/mh-stacked-carousel-widget.php
@@ -5,7 +5,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 use Elementor\Widget_Base;
 use Elementor\Controls_Manager;
-use Elementor\Group_Control_Image_Size;
 use Elementor\Utils;
 
 class MH_Stacked_Carousel_Widget extends Widget_Base {
@@ -26,15 +25,12 @@ class MH_Stacked_Carousel_Widget extends Widget_Base {
 		return [ 'mh-plug-widgets' ];
 	}
 
-	/**
-	 * Load Styles & Scripts
-	 * Note: Slick is already loaded globally by mh-plug.php
-	 */
 	public function get_script_depends() {
-		return [ 'mh-slick-js' ]; // Ensure Slick is available
+		return [ 'mh-slick-js' ];
 	}
 
 	protected function register_controls() {
+		// Content Controls
 		$this->start_controls_section(
 			'content_section',
 			[
@@ -67,7 +63,6 @@ class MH_Stacked_Carousel_Widget extends Widget_Base {
 					[ 'image' => [ 'url' => Utils::get_placeholder_image_src() ] ],
 					[ 'image' => [ 'url' => Utils::get_placeholder_image_src() ] ],
 					[ 'image' => [ 'url' => Utils::get_placeholder_image_src() ] ],
-					[ 'image' => [ 'url' => Utils::get_placeholder_image_src() ] ],
 				],
 				'title_field' => 'Slide',
 			]
@@ -75,6 +70,7 @@ class MH_Stacked_Carousel_Widget extends Widget_Base {
 
 		$this->end_controls_section();
 
+		// Style Controls
 		$this->start_controls_section(
 			'style_section',
 			[
@@ -105,28 +101,6 @@ class MH_Stacked_Carousel_Widget extends Widget_Base {
 			]
 		);
 
-		$this->add_control(
-			'slide_width',
-			[
-				'label'      => __( 'Card Width', 'mhds-plug' ),
-				'type'       => Controls_Manager::SLIDER,
-				'size_units' => [ 'px' ],
-				'range'      => [
-					'px' => [
-						'min' => 200,
-						'max' => 600,
-					],
-				],
-				'default'    => [
-					'unit' => 'px',
-					'size' => 400,
-				],
-				'selectors'  => [
-					'{{WRAPPER}} .mh-stacked-item' => 'width: {{SIZE}}{{UNIT}};',
-				],
-			]
-		);
-
 		$this->end_controls_section();
 	}
 
@@ -137,7 +111,6 @@ class MH_Stacked_Carousel_Widget extends Widget_Base {
 			return;
 		}
 
-		// Unique ID for this widget instance to avoid conflicts
 		$id = 'mh-stacked-slider-' . $this->get_id();
 		?>
 
@@ -160,7 +133,7 @@ class MH_Stacked_Carousel_Widget extends Widget_Base {
 		jQuery(document).ready(function($) {
 			var $slider = $('#<?php echo esc_attr( $id ); ?>');
 
-			// 1. Image Conversion Function (as requested)
+			// 1. Image Conversion
 			$slider.find('.mh-stacked-item').each(function() {
 				var $img = $(this).find('img');
 				var src = $img.attr('src');
@@ -174,42 +147,36 @@ class MH_Stacked_Carousel_Widget extends Widget_Base {
 				}
 			});
 
-			// 2. Initialize Slick Slider
+			// 2. Initialize Slick with "One-by-One" Focus
 			$slider.slick({
 				centerMode: true,
-				centerPadding: '0px',
-				variableWidth: true,
-				slidesToShow: 3,
+				variableWidth: true, /* Allows custom 400px width */
+				slidesToShow: 1,     /* Strict focus on the center card */
+				slidesToScroll: 1,   /* Slide one by one */
 				arrows: false,
 				dots: false,
-				focusOnSelect: true,
 				infinite: true,
-				speed: 400,
-				cssEase: 'ease'
+				focusOnSelect: true,
+				autoplay: false,
+				autoplaySpeed: 3000,
+				speed: 1000,         /* Slow, smooth slide (1s) */
+				cssEase: 'cubic-bezier(0.25, 1, 0.5, 1)' /* Soft landing curve */
 			});
 
-			// 3. Custom Class Handling for 3D Depth
-			// Slick sets .slick-center. We need to identify Next/Prev for specific transforms.
-			// This logic runs on init and after change to ensure classes are correct.
+			// 3. Update Classes for 3D Depth
 			function updateClasses() {
-				$slider.find('.slick-slide').removeClass('prev next next-2 prev-2');
+				$slider.find('.slick-slide').removeClass('prev next');
 				var $center = $slider.find('.slick-center');
-				
-				// Identify immediate siblings
 				$center.prev('.slick-slide').addClass('prev');
 				$center.next('.slick-slide').addClass('next');
-				
-				// Identify outer slides (2 steps away)
-				$center.prev().prev('.slick-slide').addClass('prev-2');
-				$center.next().next('.slick-slide').addClass('next-2');
 			}
 
-			$slider.on('init afterChange', function(event, slick, currentSlide){
+			$slider.on('init afterChange', function(){
 				updateClasses();
 			});
 			
-			// Initial call (Slick sometimes needs a moment)
-			setTimeout(updateClasses, 50);
+			// Initial Trigger
+			setTimeout(updateClasses, 100);
 		});
 		</script>
 		<?php
